c76352c882b0be278261aec30c175c69853cdda0
user <test@user.com>
2019-06-12 13:38:30 +0800

    git commit 01

---
+++ src/main/java/com/whc/project/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.whc.project.controller;
+
+/**
+ * 这个是登陆的控制器：用来测试Git命令的
+ * @author 10156234c
+ *
+ */
+public class LoginController {
+	
+	public void login(){
+		System.out.println("Test Git");
+	}
+	
+	
+	public void loginout(){
+		System.out.println("login out !");
+	}
+
+}

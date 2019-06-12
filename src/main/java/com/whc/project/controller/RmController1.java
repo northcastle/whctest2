10a2e4415661b3413f773702d68bbc1ac06a0a10
user <test@user.com>
2019-06-12 16:38:37 +0800

    开玩笑，我要覆盖掉上一次的提交信息

---
+++ src/main/java/com/whc/project/controller/RmController1.java
@@ -0,0 +1,15 @@
+package com.whc.project.controller;
+
+
+/**
+ * 这个是用来 测试Git中的删除数据的操作
+ * @author 10156234c
+ *
+ */
+public class RmController1 {
+	
+	public void readyToRm(){
+		System.out.println("I'm ready to be removed!");
+	}
+
+}
